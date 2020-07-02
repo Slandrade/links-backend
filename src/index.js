47b47cdba328412687935dcc8bb778ef8a86ec35
Silas Leandro <silaslsouza@gmail.com>
2020-07-02 12:36:02 -0300

    Sequelize configured.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const db = require('./models');
 
 const authController = require('./controllers/auth');
 
@@ -10,6 +11,8 @@ app.get('/', (request, response) => {
     return response.json('Api Running...');
 });
 
-app.listen(3001, () => {
-    console.log('LIstening on port 3001');
+db.sequelize.sync().then(() => {
+    app.listen(3001, () => {
+        console.log('LIstening on port 3001');
+    });
 });
