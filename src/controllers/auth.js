eced2bd6db9263af4f21585a98c97dd18794f6a6
Silas Leandro <silaslsouza@gmail.com>
2020-07-02 14:21:59 -0300

    Storing accounts into database

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,12 +1,27 @@
 const express = require('express');
+const bcrypt = require('bcrypt');
+const { Account } = require('../models');
+
 const router = express.Router();
+
+const saltRounds = 10;
 
 router.get('/sign-in', (request, response) => {
     return response.json('Sign in!');
 });
 
-router.get('/sign-up', (request, response) => {
-    return response.json('Sign up!');
+router.get('/sign-up', async (request, response) => {
+
+    const email = 'silaslsouza@gmail.com';
+    const password = '123456';
+    
+    const hash = bcrypt.hashSync(password, saltRounds);
+    console.log(hash);
+
+    const result = await Account.create({ email, password: hash });
+    
+
+    return response.json(result);
 });
 
 module.exports = router;
